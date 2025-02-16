25fea735e9194b05255f01e7c15b3127a4015e7c
Arseniy Zhdanov <noob1kxgod@outlook.com>
2025-02-16 21:37:40 +0300

    This is where it all starts...

---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About me</RouterLink>
+    </nav>
+</template>
